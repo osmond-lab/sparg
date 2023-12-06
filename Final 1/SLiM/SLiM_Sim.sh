d89362927a1f2d508a34b8fe46ccac2d180c6dba
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2023-12-06 10:29:36 -0800

    cleanup
    Just cleaning up this branch to only include what we need for publication (package, figures and code to generate, and documentation)

--- Final 1/SLiM/SLiM_Sim.sh
+++
@@ -1,8 +0,0 @@
-for s_disp in $(seq 0.75 0.25 1)
-do
-for rep in {1..10}
-do 
-slim -d SIGMA_disp=$s_disp -d rep=$rep simple_space.slim;
-echo $rep
-done
-done
--- jupyter_notebooks/argutils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Various utilities for working with args.
-"""
-from . ancestry import *
-from . viz import draw
-from . convert import *
