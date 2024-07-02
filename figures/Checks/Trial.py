00e51f5b38352da88d5d5497deadac84bf1b51eb
Puneeth <puneethderaje@gmail.com>
2024-07-01 22:50:26 -0400

    checks

---
+++ figures/Checks/Trial.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 14:52:22 2024
+
+@author: puneeth
+"""
+
+import tskit 
+
+tskit.load("/home/puneeth/UofT/Projects/SpARG Project/sparg2.0-manuscript/sparg2.0/figures/final/Checks/code/slim_0.25rep1sigma.trees")
